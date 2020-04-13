2a8b5e63b390d00ede63e239af8a91c1639aceab
karenheyn <karenheyn@gmail.com>
2020-04-13 19:45:10 -0400

    added bootstrap grid to home page

--- src/home/home.js
+++ src/home/home.js
@@ -19,6 +19,32 @@ class Home extends Component {
         </h1>
         <h2 className='subhead'>The community-driven makeup website</h2>
         <div className='grid-container'>
+          <div class='row'>
+            <div class='col-sm-8 image-div'>
+              <img className='lips' src={Lips} alt='lips'></img>
+            </div>
+            <div class='col-sm-4 image-div'>
+              <img className='lips' src={Assorted} alt='lips'></img>
+            </div>
+          </div>
+          <div class='row'>
+            <div class='col-sm-4 image-div'>
+              <img className='lips' src={Gold} alt='lips'></img>
+            </div>
+            <div class='col-sm-8 image-div'>
+              <img className='lips' src={Shadow} alt='lips'></img>
+            </div>
+          </div>
+          <div class='row'>
+            <div class='col-sm-8 image-div'>
+              <img className='lips' src={Red} alt='lips'></img>{" "}
+            </div>
+            <div class='col-sm-4 image-div'>
+              <img className='lips' src={Brushes} alt='lips'></img>
+            </div>
+          </div>
+        </div>
+        {/* <div className='grid-container'>
           <div className='grid-item'>
             <img className='lips' src={Lips} alt='lips'></img>
           </div>
@@ -37,7 +63,7 @@ class Home extends Component {
           <div className='grid-item'>
             <img className='lips' src={Brushes} alt='lips'></img>
           </div>
-        </div>
+        </div> */}
       </div>
     );
   }
