972aa8de8d4e0980b3bcbd7f82e708d5c07f8a55
karenheyn <karenheyn@gmail.com>
2020-04-14 16:04:59 -0400

    added key

--- src/update/input.js
+++ src/update/input.js
@@ -12,23 +12,17 @@ class Input extends React.Component {
     this.setState({ search: e.target.value });
     console.log(this.state.value);
   };
-  // createProps = () => {
-  //   const inputProps = this.props.props;
-  //   console.log(inputProps);
-  //   const listItems = inputProps.map((number) => <li>{number}</li>);
-  //   return <ul>{listItems}</ul>;
-  // };
   render() {
-    const data = Array.from(this.props.props);
+    const data = this.props.props;
     const dataArray = data.map((item) => (
       <input
         className='form-control form-control-sm'
         name='_id'
         type='text'
         placeholder={item.placeholder}
-        // value={_id}
-
+        value={item._id}
         onChange={this.handleChange}
+        key={item.name}
       />
     ));
     return <div>{dataArray}</div>;
